1e897c4f19a00f12ed769e0f79a28d7782bfd078
Maycon Gabriel <99516274+maycongabriel28@users.noreply.github.com>
2022-02-14 23:11:56 -0300

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import numpy as np # operações matematicas com arrays
 import sklearn as sk # importa pacate scikit-learn, onde traz algoritmos e recurso de Machine Learnig
 from sklearn.linear_model import LinearRegression # Importando modulo machine learning de regressão linear
 
-from machine import dtr, modelo_v1
+from machine import machine
 import os # importa biblioteca para entrada e saída de arquivo
 
 app = Flask(__name__)
