f1829a3f305d9fc662475121b464d4b66208ebd3
Maycon Gabriel <99516274+maycongabriel28@users.noreply.github.com>
2022-02-16 22:25:35 -0300

    update

--- app.py
+++ app.py
@@ -62,7 +62,7 @@ def resultado():
 #if __name__=="__main__": # Verifica se __name__ esta contido neste script
     #app.run(debug=True) # Executa este script, definindo endereço do servidor que ira rodar a aplicação e a porta
 if __name__=='main':
-    port = int(os.getenv('PORT'), '5000')
+    port = int(os.getenv('PORT'), '5050')
     app.run(host='0.0.0.0', port = port)
 
 if __name__=="__main__": # Verifica se __name__ esta contido neste script
